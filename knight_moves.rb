2527f7cb3437acbd92f64a248ee24353fc627afa
Thomas Noe <thomasjnoe@gmail.com>
2014-08-13 00:51:31 -0400

    Added comments

--- knight_moves.rb
+++ knight_moves.rb
@@ -1,3 +1,5 @@
+# Class to hold the current position and path while traversing possible moves
+# TO-DO: Change to Struct
 class BoardSquare 								
 	attr_accessor :position, :path	
 	def initialize(position, path)
@@ -7,16 +9,23 @@ class BoardSquare
 end
 
 def knight_moves(start, finish)
+	# Check for invalid starting or finishing positions, and if start and finish are the same
 	return "You entered an invalid starting position" unless is_valid_position?(start) 
 	return "You entered an invalid ending position" unless is_valid_position?(finish)
 	return "You're already on that position!" if start == finish
+
 	queue = [BoardSquare.new(start, [start])] 
 	visited = [start]
-	until queue.empty?
-		current = queue.shift
 
+	until queue.empty?
+		# Make the first BoardSquare in the queue the current BoardSquare
+		current = queue.shift 
+
+		# Find all possible moves for the current position that have not already been visited
 		possible_moves = possible_moves(current.position).select { |move| !visited.include?(move) }
 		
+		# If the current position's possible moves includes the finishing position, add the finishing position
+		# to the current path, display the amount of moves and the moves themselves, and return
 		if possible_moves.include?(finish)
 			current.path << finish
 			if current.path.size-1 == 1
@@ -28,6 +37,9 @@ def knight_moves(start, finish)
 			return
 		end
 		
+		# If the finishing position is not in the current possible moves list, 
+		# add each possible move to the queue and create a new BoardSquare with
+		# an updated path
 		possible_moves.each do |move|
 			queue << BoardSquare.new(move, (current.path + [move]))
 			visited << move
@@ -36,6 +48,8 @@ def knight_moves(start, finish)
 	end
 end
 
+# Create the possible moves for a given position on the board and remove
+# invalid moves
 def possible_moves(pos)
 	changes = [[-2,-1],[-2,1],[-1,-2],[-1,2],[2,-1],[2,1],[1,-2],[1,2]]
 	moves = []
@@ -43,6 +57,7 @@ def possible_moves(pos)
 	moves.select { |move| is_valid_position?(move) }
 end
 
+# Validate position would appear on an 8x8 Chess board
 def is_valid_position?(pos)
 	return (pos[0] > -1 && pos[0] < 8) && (pos[1] > -1 && pos[1] < 8) ? true : false
 end
